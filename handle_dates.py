347f25280358e0f0f24056ba0353bec22c9d1a6d
marcusna <marcusna@axis.com>
2022-07-07 21:39:33 +0200

    Last commit

--- handle_dates.py
+++ handle_dates.py
@@ -1,6 +1,7 @@
 
 from dateutil.parser import parse
 import pandas as pd
+import json
 
 
 def handle_bad_format(date: str):
@@ -14,28 +15,32 @@ def handle_bad_format(date: str):
 
 
 def main():
-    dates_train = pd.read_csv('train.csv/train.csv').pop('date')
-    dates_test = pd.read_csv('test.csv/test.csv').pop('date')
-    dates_total = pd.concat([dates_train, dates_test])
-    dates_total_copy = dates_total.copy()
+    # dates_train = pd.read_csv('train.csv/train.csv').pop('date')
+    # dates_test = pd.read_csv('test.csv/test.csv').pop('date')
+    # dates_total = pd.concat([dates_train, dates_test])
+    # dates_total_copy = dates_total.copy()
 
-    for idx, date in enumerate(dates_total):
-        date_object = parse(handle_bad_format(date))
-        hours = date_object.hour
-        weekday = date_object.weekday()
-        match weekday:
-            case 0 | 1 | 2 | 3 | 4:  # Work day
-                if hours in range(6, 19):
-                    dates_total_copy[idx] = 1
-                elif hours in range(19, 25):
-                    dates_total_copy[idx] = 2
-            case 5 | 6:  # Weekend
-                if hours in range(6, 19):
-                    dates_total_copy[idx] = 3
-                elif hours in range(19, 25):
-                    dates_total_copy[idx] = 4
+    # for idx, date in enumerate(dates_total):
+    #     date_object = parse(handle_bad_format(date))
+    #     hours = date_object.hour
+    #     weekday = date_object.weekday()
+    #     match weekday:
+    #         case 0 | 1 | 2 | 3 | 4:  # Work day
+    #             if hours in range(6, 19):
+    #                 dates_total_copy[idx] = 1
+    #             elif hours in range(19, 25):
+    #                 dates_total_copy[idx] = 2
+    #         case 5 | 6:  # Weekend
+    #             if hours in range(6, 19):
+    #                 dates_total_copy[idx] = 3
+    #             elif hours in range(19, 25):
+    #                 dates_total_copy[idx] = 4
 
-    print(dates_total_copy)
+    # print(dates_total_copy)
+    json_file_path = "dates.json"
+    with open(json_file_path, 'r') as j:
+        contents = json.loads(j.read())
+    print(contents['weekdays'])
 
 
 if __name__ == "__main__":
